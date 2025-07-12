4d6aa18e9ffda609e5f6f6d759fb4652684f90ce
Noumbissi16 <noumbissistael@gmail.com>
2025-07-12 10:19:16 +0100

    Cocktails section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Art from "./components/Art";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Menu from "./components/Menu";
-import { SplitText, ScrollTrigger} from "gsap/all"
+import { SplitText, ScrollTrigger } from "gsap/all";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 function App() {
-
   return (
     <main>
       <Navbar />
       <Hero />
-      <Menu />
+      <Cocktails />
+      {/* <Menu /> */}
       {/* <About /> */}
       {/* <Contact /> */}
       {/* <Art /> */}
